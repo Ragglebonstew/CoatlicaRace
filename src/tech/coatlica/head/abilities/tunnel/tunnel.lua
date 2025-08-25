5f0d6ba8ff49c9bd60abd6c91b12c402ae1935d6
Ragglebonstew <145236701+Ragglebonstew@users.noreply.github.com>
2025-08-25 01:04:07 -0400

    Fixed tunnel ability disabling gravity unintentionally

--- src/tech/coatlica/head/abilities/tunnel/tunnel.lua
+++ src/tech/coatlica/head/abilities/tunnel/tunnel.lua
@@ -5,21 +5,17 @@ function Tunnel:uninit() end
 
 function Tunnel:update(dt, dir, shiftHeld)
 
-	local inGround = false
-	
-	if mcontroller.zeroG() then
-		inGround = true
-	else
-		local headPoly = { {0.75, 1.0}, {1.0, 0.75}, {1.0, -0.75}, {0.75, -1.0}, {-0.75, -1.0}, {-1.0, -0.75}, {-1.0, 0.75}, {-0.75, 1.0} }
-		local playerPos = mcontroller.position()
-		inGround = world.polyCollision(headPoly, playerPos, {"Block", "Platform", "Dynamic", "Slippery", "Null"})
+	local headPoly = { {0.75, 1.0}, {1.0, 0.75}, {1.0, -0.75}, {0.75, -1.0}, {-0.75, -1.0}, {-1.0, -0.75}, {-1.0, 0.75}, {-0.75, 1.0} }
+	local playerPos = mcontroller.position()
+	inGround = world.polyCollision(headPoly, playerPos, {"Block", "Platform", "Dynamic", "Slippery", "Null"})
+	if inGround then
+		mcontroller.controlParameters({gravityEnabled = true})
 	end
 	
 	mcontroller.controlParameters({
-		gravityEnabled = not inGround,
 		collisionEnabled = false,
-		airFriction = 0,
-		liquidFriction = 0,
-		liquidBuoyancy = 0.0
+		airFriction = 0.1,
+		liquidFriction = 0.1,
+		liquidBuoyancy = 0.1
 	})
 end
