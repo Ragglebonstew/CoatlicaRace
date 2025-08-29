c4c263e589dfc2ab445be8915078535d7c4e5a2c
Ragglebonstew <145236701+Ragglebonstew@users.noreply.github.com>
2025-08-28 20:35:04 -0400

    Shadow ability now gives invulnerability

--- src/tech/coatlica/head/abilities/shadow/shadow.lua
+++ src/tech/coatlica/head/abilities/shadow/shadow.lua
@@ -4,15 +4,17 @@ function Shadow:init()
 	self.progress = 0
 end
 function Shadow:uninit()
-	local directives = ""
-	setDirectives(directives)
+	setDirectives()
 	self.active = false
+	status.clearPersistentEffects("coatlica_shadow_ability")
 end
 function Shadow:update(dt, dir, shiftHeld)
 	if self.active then
-		self.progress = math.min(self.progress+dt*40,100)
+		self.progress = math.min(self.progress+dt*50,100)
+		--energy
+		status.overConsumeResource("energy", self.energyCost*dt)
 	else
-		self.progress = math.max(self.progress-dt*40,0)
+		self.progress = math.max(self.progress-dt*50,0)
 	end
 	local brightnessDirective = "?brightness=-"..math.floor(self.progress*0.8)
 	local transparntDirective = "?multiply=FFFFFF"..string.format("%X", math.floor((1-self.progress/100*0.8)*255))
@@ -21,8 +23,10 @@ function Shadow:update(dt, dir, shiftHeld)
 end
 function Shadow:fire()
 	self.active = true
+	status.setPersistentEffects("coatlica_shadow_ability", {{stat = "invulnerable", amount = 1}})
 end
 function Shadow:hold(dt) end
 function Shadow:release(headId)
 	self.active = false
+	status.clearPersistentEffects("coatlica_shadow_ability")
 end
