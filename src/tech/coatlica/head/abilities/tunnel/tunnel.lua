0f886129b2b1bddfe5174680f5b145583e119e58
Ragglebonstew <145236701+Ragglebonstew@users.noreply.github.com>
2025-09-07 10:46:36 -0400

    Less weird tunnel collision

--- src/tech/coatlica/head/abilities/tunnel/tunnel.lua
+++ src/tech/coatlica/head/abilities/tunnel/tunnel.lua
@@ -7,9 +7,9 @@ function Tunnel:uninit() end
 
 function Tunnel:update(dt, dir, shiftHeld)
 
-	local headPoly = { {0.75, 1.0}, {1.0, 0.75}, {1.0, -0.75}, {0.75, -1.0}, {-0.75, -1.0}, {-1.0, -0.75}, {-1.0, 0.75}, {-0.75, 1.0} }
+	--local headPoly = { {0.75, 1.0}, {1.0, 0.75}, {1.0, -0.75}, {0.75, -1.0}, {-0.75, -1.0}, {-1.0, -0.75}, {-1.0, 0.75}, {-0.75, 1.0} }
 	local playerPos = mcontroller.position()
-	inGround = world.polyCollision(headPoly, playerPos, {"Block", "Platform", "Dynamic", "Slippery", "Null"})
+	inGround = world.pointCollision(playerPos, {"Block", "Platform", "Dynamic", "Slippery", "Null"})
 	if inGround then
 		mcontroller.controlParameters({gravityEnabled = true})
 		local vel = mcontroller.velocity()
